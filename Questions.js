ffd563df0e042a507612060b7cda57309b300177
Chloe <144443749+chloeg07@users.noreply.github.com>
2024-11-04 12:00:30 +0000

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,38 @@
+//1.
+let a = 7;
+let b = 8.5;
+let c = 9;
+console.log(a, b, c);
+
+a = a+b;
+b = b+c;
+
+console.log(a, b, c)
+
+//2.
+let no = 7;
+console.log(no,no+1,no+2)
+
+//3.
+let radius = 5;
+let PI = 3.14;
+let blah = PI * radius * radius;
+console.log("Area of the circle:", blah);
+
+//4.
+let base = 10;
+let height = 5;
+
+let area = 0.5 * base * height;
+console.log("Area of the triangle:",area);
+
+//5.
+let n = 2;
+console.log("n:", n);
+console.log("n^2:",n ** 2);
+console.log("n^3:", n ** 3);
+console.log("n^4:", n ** 4);
+
+//6.
+let heightCm = 100;
+let heightFt = heightCm /2.4 /12;
